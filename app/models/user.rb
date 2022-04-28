ca21be22fb635fcb45fe1c3f5a0c9fa6664de7a7
emfy0 <pasha.egorov.02.p@ya.ru>
2022-04-29 00:10:38 +0300

    added rus messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, :nickname, :name, presence: true
+  validates :email, :nickname, :name, presence: { message: 'не может быть пустым!' }
 
   validates :nickname, { uniqueness: { message: '%{value} уже занят!' },
                          format: { with: /\A[a-zA-Z_1-9]+\z/,
