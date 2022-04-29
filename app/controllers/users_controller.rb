3cfb778312759575b70305f2d2b6196c11a7b96d
emfy0 <pasha.egorov.02.p@ya.ru>
2022-04-29 19:27:23 +0300

    added edit for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
-
     @user = User.new(user_params)
 
     if @user.save
@@ -17,5 +15,28 @@ class UsersController < ApplicationController
 
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Данные пользователя обновлены!'
+    else
+      flash.now[:alert] = 'Проверьте форму!'
+
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
-  
+
   resources :questions
   resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
 end
