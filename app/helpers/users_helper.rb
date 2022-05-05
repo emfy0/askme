184a3c072a3b640d09205554211f25ecca8807c1
emfy <moonmeander47@ya.ru>
2022-05-06 00:08:44 +0300

    author link rework

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,15 +3,11 @@ module UsersHelper
     "@#{str}"
   end
 
-  def get_nickname_by_id(id)
-    anonim = content_tag(:span, '@аноним', class: 'mr-sm text-underline')
-    return anonim if id.nil?
-
-    user = User.find(id)
-    if user.present?
-      link_to add_at(user.nickname), user_path(user), class: 'nickname mr-sm'
+  def author_link(author)
+    if author.present?
+      link_to add_at(author.nickname), user_path(author), class: 'nickname mr-sm'
     else
-      anonim
+      content_tag(:span, '@аноним', class: 'mr-sm text-underline')
     end
   end
 end
