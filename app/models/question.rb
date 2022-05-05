63179c037d7d53d2f2f587688eb4769f3d639aea
emfy <moonmeander47@ya.ru>
2022-05-05 23:57:55 +0300

    fixed associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ApplicationRecord
   validates :body, { presence: true,
                      length: { maximum: 280 } }
   belongs_to :user
-  belongs_to :author, class_name: :User, foreign_key: :user_id
+  belongs_to :author, class_name: :User, optional: true
 end
